23f42f935e361f67807b9787d8dd22e22ccd38d7
g-batista <gbatista@madisoncollege.edu>
2022-11-16 21:12:52 -0500

    testing connection

--- footer.php
+++ footer.php
@@ -15,7 +15,6 @@
     &#169;2022
   </div>
 </footer>
-
  <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
